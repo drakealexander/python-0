a24b83ece837361e4207b8ed375f06cf5aa65201
Drake Griffith <drakegriffith8@gmail.com>
2015-04-28 18:59:52 -0400

    save

--- colors.py
+++ colors.py
@@ -20,3 +20,37 @@ cyan  =  '\033[0;36m'
 green =  '\033[0;32m'
 reset  = '\033[0m'
 clear =  '\033[H\033[2J'
+
+pink = magenta
+
+import random
+def random_color():
+    return random.choice([yellow, orange, red, magenta, violet, blue, cyan, green])
+
+if __name__ == '__main__':
+    print(clear)
+    print(base03 + 'Base03' + reset, end=' ')
+    print(base02 + 'Base02' + reset, end=' ')
+    print(base01 + 'Base01' + reset, end=' ')
+    print(base00 + 'Base00' + reset, end=' ')
+    print(base0 + 'Base0' + reset, end=' ')
+    print(base1 + 'Base1' + reset, end=' ')
+    print(base2 + 'Base2' + reset, end=' ')
+    print(base3 + 'Base3' + reset, end=' ')
+
+    print()
+    
+    print(yellow + 'Yellow' + reset, end=' ')
+    print(orange + 'Orange' + reset, end=' ')
+    print(red + 'Red' + reset, end=' ')
+    print(magenta + 'Magenta' + reset, end=' ')
+    print(violet + 'Violet' + reset, end=' ')
+    print(blue + 'Blue' + reset, end=' ')
+    print(cyan + 'Cyan' + reset, end=' ')
+    print(green + 'Green' + reset, end=' ')
+
+    print()
+
+    for count in range(7):
+        print(random_color() + 'Random' + reset, end=' ')
+    print()
